aa4f40df6b2b28665ac32cd7a8c3022a4c9075c3
Axi <axidreamdragon@gmail.com>
2024-09-20 16:01:23 +0200

    Add model rotation, add model texture

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Timer from './components/Timer';
 import TaskList from './components/TaskList';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
+import { useRef } from 'react';
+import { createNoise3D } from 'simplex-noise';
+import * as THREE from 'three';
+
+const noise = createNoise3D();
 
 function App() {
   return (
     <div className="App">
       <div>
         <Canvas>
-          <ambientLight intensity={25} />
-          <pointLight position={[2, 1, 1]} intensity={25} />
+          <ambientLight intensity={5} />
           <D20 />
           <OrbitControls />
         </Canvas>
@@ -22,14 +26,26 @@ function App() {
 
 function D20() {
   const { scene } = useGLTF('/assets/models/d20.glb', true);
+  const ref = useRef();
 
   scene.traverse((child) => {
     if (child.isMesh) {
-      child.material.color.set('#ff5555');
+      child.material.flatShading = true;
+      child.material.lights = false;
+      child.material.needsUpdate = true;
     }
   });
 
-  return <primitive object={scene} />;
+  useFrame(({ clock }) => {
+    if (ref.current) {
+      const time = clock.getElapsedTime() / 25;
+      ref.current.rotation.x = noise(time, 0, 0) * Math.PI;
+      ref.current.rotation.y = noise(0, time, 0) * Math.PI;
+      ref.current.rotation.z = noise(0, 0, time) * Math.PI;
+    }
+  });
+
+  return <primitive ref={ref} object={scene} />;
 }
 
 export default App;
