636991387e8c128784856a06a1dfdafc5bd15b01
Axi <axidreamdragon@gmail.com>
2024-09-22 14:47:19 +0200

    Create website structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Timer from './components/Timer';
 import TaskList from './components/TaskList';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <Timer />
-      <TaskList />
+    <div className={styles.App}>
+      <div className={styles.top}>
+        <div>
+          info
+        </div>
+        <Timer />
+        <div>
+          task creator
+        </div>
+      </div>
+      <div className={styles.bottom}>
+        <TaskList />
+      </div>
     </div>
   );
 }
